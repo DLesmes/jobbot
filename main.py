d85948b637c25e5f2c352371ef575eaaf0ef46e7
Usuario <ing.dlesmes@gmail.com>
2025-02-27 17:04:49 -0500

    ✊ thusday 27 of 2025 recommendations run

--- main.py
+++ main.py
@@ -2,13 +2,7 @@
 # repo imports
 from src.app.controllers.seeker import Seeker
 seeker = Seeker()
-from src.app.services.expirer import Expirer
-expirer = Expirer()
-from src.app.services.preprocesor import Preprocesor
-preprocesor = Preprocesor()
 
 if __name__== '__main__':
-    #preprocesor.run()
-    #expirer.update()
     seeker.run()
     
