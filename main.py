fae0d6ddac76bc0f96ba0b5911de76dcb0531936
Usuario <ing.dlesmes@gmail.com>
2025-03-04 10:36:26 -0500

    🕯️ adding the remote knowledge filter

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 """ main file for recommendations """
 # repo imports
-from src.app.controllers.seeker import Seeker
-seeker = Seeker()
+#from src.app.controllers.seeker import Seeker
+#seeker = Seeker()
+from src.app.services.mentor import Mentor
+mentor = Mentor()
 
 if __name__== '__main__':
-    seeker.run()
+    #seeker.run()
+    mentor.save_matches()
--- src/app/services/mentor.py
+++ src/app/services/mentor.py
@@ -31,6 +31,7 @@ class Mentor():
         seniority_criteria = user[0]['seniority']
         location_criteria = user[0]['location']
         work_modality_criteria = user[0]['work_modality_english']
+        remote_criteria = [ast.literal_eval(val) for val in user[0]['remote']]
         excluded_companies = self.filter_params
         # offers
         list_jobs = open_json(self.job_offers)
@@ -39,6 +40,7 @@ class Mentor():
             (df_jobs["seniority"].isin(seniority_criteria)) &  # Filter by seniority
             (df_jobs["location"].isin(location_criteria)) &    # Filter by location
             (df_jobs["work_modality_english"].isin(work_modality_criteria)) &  # Filter by work modality
+            (df_jobs["remote"].isin(remote_criteria)) &  # Filter by remote_criteria
             (~df_jobs["company"].isin(excluded_companies))     # Exclude specified companies
         ].copy()
         print(f'current available jobs: {df_filtered.shape}')
@@ -51,26 +53,29 @@ class Mentor():
         for _ , row in df_users.iterrows():
             knowledge_filted_job_id = self.knowledge_based_filter(row['user_id'])
             df_jobs = df_jobs[df_jobs['job_id'].isin(knowledge_filted_job_id)]
-            list_dict_jobs = df_jobs.to_dict(orient='records')
-            list_dict_jobs_scored = [
-                {
-                    **job,
-                    'score': cosine_similarity_numpy(
-                        job['embed'],
-                        row['embed']
-                    )
-                }
-                for job in list_dict_jobs
-            ]
-            df_matches = pd.DataFrame(list_dict_jobs_scored)
-            print(f'{'#'*10} user scores: {row['user_id']}\n',df_matches.score.value_counts)
-            df_matches = df_matches[df_matches['score']>=self.similarity_threshold].copy()
-            df_matches['match_id'] = row['user_id']+df_matches['job_id']
-            df_matches['match_date'] = datetime.today().strftime("%Y-%m-%d")
-            df_matches = df_matches[['match_id','match_date','score']].copy()
-            tmp_dict_matches = df_matches.to_dict(orient='records')
-            dict_matches = dict_matches + tmp_dict_matches
-        return dict_matches
+            if df_jobs.shape[0]>0:
+                list_dict_jobs = df_jobs.to_dict(orient='records')
+                list_dict_jobs_scored = [
+                    {
+                        **job,
+                        'score': cosine_similarity_numpy(
+                            job['embed'],
+                            row['embed']
+                        )
+                    }
+                    for job in list_dict_jobs
+                ]
+                df_matches = pd.DataFrame(list_dict_jobs_scored)
+                print(f'{'#'*10} user scores: {row['user_id']}\n',df_matches.score.value_counts)
+                df_matches = df_matches[df_matches['score']>=self.similarity_threshold].copy()
+                df_matches['match_id'] = row['user_id']+df_matches['job_id']
+                df_matches['match_date'] = datetime.today().strftime("%Y-%m-%d")
+                df_matches = df_matches[['match_id','match_date','score']].copy()
+                tmp_dict_matches = df_matches.to_dict(orient='records')
+                dict_matches += tmp_dict_matches
+                return dict_matches
+            else:
+                print(f'There are no matches for knowledge filter for the user {row['user_id']}')
     
     def save_matches(self):
         dict_matches = self.recommend()
