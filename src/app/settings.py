74e6a01c6e0a808a647c7f236d58bac8b5d0666b
Diego Lesmes <61529697+DLesmes@users.noreply.github.com>
2025-03-11 11:06:55 -0500

    ContextFilter setting up to implement logger

--- src/app/settings.py
+++ src/app/settings.py
@@ -1,3 +1,6 @@
+""" Env variables and general config file """
+import logging
+import inspect
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -20,3 +23,41 @@ class Settings:
     RETRY_DELAY_SECONDS = os.environ["RETRY_DELAY_SECONDS"]
     MAX_RETRIES = os.environ["MAX_RETRIES"]
     ROLE_WEIGHT = os.environ["ROLE_WEIGHT"]
+
+# Custom filter to add class and method information
+class ContextFilter(logging.Filter):
+    def filter(self, record):
+        # Get the current frame (caller of the logger)
+        frame = inspect.currentframe().f_back.f_back  # 2 levels up
+        # Get the class name (if called from a class method)
+        record.class_name = 'N/A'
+        if 'self' in frame.f_locals:
+            record.class_name = frame.f_locals['self'].__class__.__name__
+        # Get the method/function name
+        record.method_name = frame.f_code.co_name
+        return True
+
+# Configure logging
+def setup_logging():
+    logger = logging.getLogger('my_app')
+    logger.setLevel(logging.INFO)
+
+    # Create a console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+
+    # Create a formatter with class and method information
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(class_name)s - %(method_name)s - %(message)s'
+    )
+    console_handler.setFormatter(formatter)
+
+    # Add the custom filter to the logger
+    context_filter = ContextFilter()
+    logger.addFilter(context_filter)
+
+    # Add the handler to the logger
+    logger.addHandler(console_handler)
+
+# Set up logging
+setup_logging()
